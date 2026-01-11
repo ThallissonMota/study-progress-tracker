591a1ded9ca797f9b240c40e0cd11d359f983ee2
Thallisson Mota <thallissonmota2@gmail.com>
2026-01-10 23:01:39 -0300

    Delete src directory

--- src/separador.py
+++
@@ -1,8 +0,0 @@
-def separador(texto = "", tamanho = 60):
-    """
-    Exibe um separador visual opcional com texto centralizado.
-    """
-    if texto:
-        print(f"\n{"=" * 25} ({texto}) {"=" * 25}\n")
-    else:
-        print(f"\n{"=" * tamanho}\n")
